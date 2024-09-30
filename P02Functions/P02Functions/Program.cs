ea0a1f04caf80fbbd5360d5e2d0b9754d38efb7c
SirBucketT <tkpriva@icloud.com>
2024-09-30 13:28:14 +0200

    part 1 of 2 done

---
+++ P02Functions/P02Functions/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exercises 2 - Functions");
+
+void moveForward()
+{
+    Console.WriteLine("Moving forward");
+}
+void turnRight()
+{
+    Console.WriteLine("Turning right");
+}
+
+moveForward(); moveForward(); moveForward();
+turnRight(); turnRight();
+moveForward(); moveForward(); moveForward();
+turnRight(); turnRight(); turnRight();
+moveForward(); moveForward(); moveForward();
+turnRight(); turnRight(); turnRight();
+moveForward(); 
+turnRight(); turnRight();
+moveForward(); 
