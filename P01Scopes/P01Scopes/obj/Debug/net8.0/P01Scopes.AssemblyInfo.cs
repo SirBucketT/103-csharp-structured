9488207fd379e0c9a0afbf41fbbc2b38024d178d
SirBucketT <tkpriva@icloud.com>
2024-09-30 13:25:55 +0200

    part 1 of assignment 2 done

--- P01Scopes/P01Scopes/obj/Debug/net8.0/P01Scopes.AssemblyInfo.cs
+++ P01Scopes/P01Scopes/obj/Debug/net8.0/P01Scopes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P01Scopes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f6e79f03f01017e63668b65fb5fb389fda2b83")]
 [assembly: System.Reflection.AssemblyProductAttribute("P01Scopes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P01Scopes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
