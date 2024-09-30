86f6e79f03f01017e63668b65fb5fb389fda2b83
SirBucketT <tkpriva@icloud.com>
2024-09-30 11:25:36 +0200

    first part done

---
+++ P01Scopes/P01Scopes/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Give me a number.");
+{
+    string input = Console.ReadLine();
+    int increment = 0;
+    increment++;
+    Console.WriteLine($"{input}{increment}");
+}
+{
+    Console.WriteLine("Give me another number.");
+    
+    int input = int.Parse(Console.ReadLine());
+    input++;
+    Console.WriteLine(input);
+}
+{
+    Console.WriteLine("Give me another number.");
+    
+    char input = Convert.ToChar(Console.ReadLine());
+    input++;
+    Console.WriteLine(input);
+}
