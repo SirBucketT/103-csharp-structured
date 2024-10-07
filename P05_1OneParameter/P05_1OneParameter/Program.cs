7933effc45d85728cd070d152b64f93d1e5fc707
SirBucketT <tkpriva@icloud.com>
2024-10-07 10:15:18 +0200

    part 1 of 5th part done

---
+++ P05_1OneParameter/P05_1OneParameter/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Input a number and I'll give you the fibonacci of said number");
+
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Fibonacci({n}) = {Fibonacci(n)}");
+
+static long Fibonacci(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+    
