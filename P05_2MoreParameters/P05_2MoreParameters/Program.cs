cac7e7eeb18f42f9870392af71487e849b9efd13
SirBucketT <tkpriva@icloud.com>
2024-10-08 11:09:11 +0200

    part 5 progress

--- P05_2MoreParameters/P05_2MoreParameters/Program.cs
+++ P05_2MoreParameters/P05_2MoreParameters/Program.cs
@@ -46,3 +46,32 @@ void avNum(int num1, int num2, int result)
     result = num1 + num2 / 2;
     Console.WriteLine(result);
 }
+
+//Next part of assignment 
+
+/*Write a function that takes two integer arguments and prints the first argument to the power of the second number. Test it with the following arguments:
+
+3, 2
+5, 1
+2, 5
+2, 10
+19, 0
+13, 1
+*/ 
+
+Console.WriteLine("P05_2_2Power");
+
+
+Console.WriteLine("Give me a number");
+int powNum1 = int.Parse(Console.ReadLine());
+
+Console.Write(" Give me another number to do the power of");
+int powNum2 = int.Parse(Console.ReadLine());
+
+powNum(powNum1, powNum2);
+
+void powNum(int powNum1, int powNum2)
+{
+    double powResult = Math.Pow(powNum1, powNum2);
+    Console.WriteLine(powResult);
+}
