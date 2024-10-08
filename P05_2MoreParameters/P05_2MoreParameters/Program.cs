fba4c15f4cb2a37972d214be04a54c3377fce818
SirBucketT <tkpriva@icloud.com>
2024-10-08 10:53:19 +0200

    part 5 progress

--- P05_2MoreParameters/P05_2MoreParameters/Program.cs
+++ P05_2MoreParameters/P05_2MoreParameters/Program.cs
@@ -21,3 +21,28 @@ void lastName(string l)
 {
     Console.Write(l);
 }
+
+//Next Assignment
+/* Write a function that takes two integer arguments and prints the average of both numbers. Test it with the following arguments:
+
+2, 6
+0, 10
+-12, 12
+11, 9
+*/ 
+
+Console.WriteLine("P05_2_1Average");
+
+Console.WriteLine("Give me a number");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Give me another number and I'll tell you the average of them");
+int num2 = int.Parse(Console.ReadLine());
+int result = 0;
+
+avNum(num1, num2, result);
+
+void avNum(int num1, int num2, int result)
+{
+    result = num1 + num2 / 2;
+    Console.WriteLine(result);
+}
--- P05_2MoreParameters/P05_2MoreParameters/obj/Debug/net8.0/P05_2MoreParameters.AssemblyInfo.cs
+++ P05_2MoreParameters/P05_2MoreParameters/obj/Debug/net8.0/P05_2MoreParameters.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P05_2MoreParameters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+132740dc619a9887428a31dde3c2b2289f2b7242")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8d7ee9d1888d13850e835ece21d030b153a933")]
 [assembly: System.Reflection.AssemblyProductAttribute("P05_2MoreParameters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P05_2MoreParameters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
