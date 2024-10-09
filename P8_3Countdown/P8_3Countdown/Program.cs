fb320c536e645a59601246bfc70beae4b5c41d33
SirBucketT <tkpriva@icloud.com>
2024-10-09 17:23:23 +0200

    all of 8 done

---
+++ P8_3Countdown/P8_3Countdown/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("P8_3Countdown");
+Console.WriteLine("I'm counting down!");
+
+int countdown = 10;
+
+for (int i = 0; i < 11; i++)
+{
+    Console.Write($"{countdown}..");
+    countdown--;
+}
