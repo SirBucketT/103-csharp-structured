11be83241a13b8040b64cb519eb06af43de555d0
SirBucketT <tkpriva@icloud.com>
2024-10-09 15:48:50 +0200

    7 progress

---
+++ P7DoWhileLoops/P7DoWhileLoops/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+void AskNumberBetween()
+{
+    int min = 1;
+    int max = 3;
+    
+    int value = int.Parse(Console.ReadLine());
+    
+    
+}
