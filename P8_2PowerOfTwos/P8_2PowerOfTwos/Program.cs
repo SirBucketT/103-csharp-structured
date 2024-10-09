96c9253198f00f0d5aa27535d701445722adef4e
SirBucketT <tkpriva@icloud.com>
2024-10-09 17:18:57 +0200

    part 2 of 8 done

---
+++ P8_2PowerOfTwos/P8_2PowerOfTwos/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int powerOfStart = 1;
+
+// for (int i = 0; i < 11; i++)
+// {
+//     Console.Write($"{powerOfStart}-");
+//     int powerOf = 2;
+//     int powerOfResult = (int)Math.Pow(powerOfStart, powerOf);
+//     powerOfStart = powerOfResult;
+//     
+//     int powerOfLoop = 2;
+//     int powerOfTimes = powerOfStart;
+//     powerOfStart = powerOfTimes * powerOfLoop;
+
+//}
+Console.Write($"{powerOfStart}");
+
+for (int i = 0; i < 10; i++){
+    int result = powerOfStart * 2;
+    
+    powerOfStart = result;
+    Console.Write($"-{powerOfStart}");
+}
