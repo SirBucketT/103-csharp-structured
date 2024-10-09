10992a508a9cc7717c960f26f70bdf0e5efb4d68
SirBucketT <tkpriva@icloud.com>
2024-10-09 16:37:48 +0200

    first part of 8 done

---
+++ P8_1RandomNumbers/P8_1RandomNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("P8_1RandomNumbers");
+Console.WriteLine("Here is 13 random numbers (1-6):");
+
+Random random = new Random();
+
+for (int i = 0; i < 13; i++)
+{
+    int number;
+    number = random.Next(1, 7);
+    Console.Write(number);
+}
