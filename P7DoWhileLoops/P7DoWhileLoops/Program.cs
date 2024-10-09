e059e61d69e17a84ead0f3bd0de962418b397646
SirBucketT <tkpriva@icloud.com>
2024-10-09 16:27:47 +0200

    part 7 done

--- P7DoWhileLoops/P7DoWhileLoops/Program.cs
+++ P7DoWhileLoops/P7DoWhileLoops/Program.cs
@@ -1,13 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-
+AskNumberBetween();
 void AskNumberBetween()
 {
     int min = 1;
     int max = 3;
-    
-    int value = int.Parse(Console.ReadLine());
-    
-    
+    int value = 0;
+
+    int nextNumber = 0;
+    do
+    {
+       Console.WriteLine($"Please give me a number between {min} and {max}."); 
+       value = int.Parse(Console.ReadLine());
+       if (value >= min && value <= max)
+       {
+           nextNumber++;
+       }
+    }
+    while (nextNumber != 1);
+    {
+        Console.WriteLine($"You gave number {value}.");
+        min = 41;
+        max = 103;
+        
+        do
+        {
+            Console.WriteLine("Please give me a number between 41 and 103."); 
+            value = int.Parse(Console.ReadLine());
+            if (value >= min && value <= max)
+            {
+                nextNumber++;
+            }
+            
+        }
+        while (nextNumber != 2);
+        {
+            Console.WriteLine($"You gave number {value}.");
+        }
+    }
 }
--- P7DoWhileLoops/P7DoWhileLoops/obj/Debug/net8.0/P7DoWhileLoops.AssemblyInfo.cs
+++ P7DoWhileLoops/P7DoWhileLoops/obj/Debug/net8.0/P7DoWhileLoops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P7DoWhileLoops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd28118cf54b45e8f5f24816bbca2f1eba7ce6cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11be83241a13b8040b64cb519eb06af43de555d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("P7DoWhileLoops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P7DoWhileLoops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
