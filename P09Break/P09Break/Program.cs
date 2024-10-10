fa023efea9e53fb2d2dcddaea1c3c2edf44ef23f
SirBucketT <tkpriva@icloud.com>
2024-10-10 10:09:07 +0200

    part 9 done

---
+++ P09Break/P09Break/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int health = 10;
+
+while (health > 0)
+{
+    Console.WriteLine("Select how many lives you want to take.");
+    
+    string playerTakes = Console.ReadLine();
+    if (playerTakes == "Stop" || playerTakes == "stop")
+    {
+        goto endGame;
+    }
+    int amount = int.Parse(playerTakes);
+    
+    int damage = health - amount;
+    health = damage;
+    
+    Console.WriteLine(health);
+    
+    if (health == 0)
+    {
+        break;
+    }
+}
+endGame:
+Console.WriteLine("Game finnished");
